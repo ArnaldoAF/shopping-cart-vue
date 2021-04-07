437939f814d29981ce96e08a2ee1fc30159a673b
Arnaldo Assis Ferreira <arnaldo.ferreira@cadmus.com>
2021-04-07 10:19:26 -0300

    Actions

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,31 +1,29 @@
 <template>
   <div>
       <h1>Product List</h1>
-      <ul>
+      <h1 v-if="loading">LOADING...</h1>
+      <ul v-else>
           <li v-for="product in products" :key="product.id">{{product.title}} - {{product.price}}</li>
       </ul>
   </div>
 </template>
 
 <script>
-import shop from '@/api/shop';
 import store from '@/store/index';
 export default {
-    // data() {
-    //     return {
-    //         products: []
-    //     }
-    // },
+    data() {
+        return {
+            loading: false
+        }
+    },
     computed: {
         products() {
             return store.getters.availableProducts;
         }
     },
     created() {
-        shop.getProducts(products => {
-            //this.products = products
-            store.commit('setProducts', products);
-        })
+        this.loading = true;
+        store.dispatch('fetchProducts').then(() => this.loading = false)
     }
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import Vuex from "vuex";
-
+import shop from "@/api/shop";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
@@ -10,19 +10,30 @@ export default new Vuex.Store({
   },
   getters: {
     // = computed properties
-    productsCount() {
-      
-    },
+    productsCount() {},
     availableProducts(state, getters) {
-        return state.products.filter(product => product.inventory > 0);
+      return state.products.filter(product => product.inventory > 0);
     }
   },
   actions: {
     // = methods
-    fetchProductions() {
+    fetchProducts({commit}) {
       //make the call
-      // runb setProducts mutation
-    }
+      // run setProducts mutation
+      return new Promise((resolve, reject) => {
+        shop.getProducts(products => {
+            //this.products = products
+            commit("setProducts", products);
+            resolve();
+          });
+      })
+      
+    },
+    // actions control when the mutations are fired
+    // addToCart(context, product) {
+    //     if(product.inventory > 0)
+    //     context.commit("pushProductToCart", product)
+    // }
   },
   mutations: {
     setProducts(state, products) {
